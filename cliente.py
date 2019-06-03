42f52ddedebdc4972b7dd2c5a299bc7836752161
abelsco <abelsmenezes@gmail.com>
2019-06-03 18:15:42 -0300

    adicionado Lista de comandos

--- cliente.py
+++ cliente.py
@@ -13,27 +13,33 @@ def main(argv):
             ).strip()
             op = op.split(' ')
             if op[0] == '?':
-                print(serv.system.listMethods())
+                print('{}Lista de comandos{}'.format('*-'*10,'*-'*10))
+                print('soma [A] [B] - Soma A e B e retorna')
+                print('elevado [A] [B] - A^B')
+                print('mult [A] [B] - Multiplica A e B e retorna')
+                print('div [A] [B] - Divide A e B e retorna')
+                print('ex - Encerra a calculadora')
+                print('{}'.format('***'*20))
             elif op[0] == 'soma':
                 if len(op) == 3:
-                    print(serv.soma(int(op[1]), int(op[2])))
+                    print(serv.soma(float(op[1]), float(op[2])))
                 else:
                     print('soma A B')
             elif op[0] == 'elevado':
                 if len(op) == 3:
-                    print(serv.pow(int(op[1]), int(op[2])))
+                    print(serv.pow(float(op[1]), float(op[2])))
                 else:
-                    print('elevado A B = (A^B)')
+                    print('elevado A B')
             elif op[0] == 'mult':
                 if len(op) == 3:
-                    print(serv.mult(int(op[1]), int(op[2])))
+                    print(serv.mult(float(op[1]), float(op[2])))
                 else:
                     print('mult A B')
             elif op[0] == 'div':
                 if len(op) == 3:
-                    print(serv.div(int(op[1]), int(op[2])))
+                    print(serv.div(float(op[1]), float(op[2])))
                 else:
-                    print('mult A B')
+                    print('div A B')
             elif op[0] == 'ex':
                 print('{}Saindo{}'.format('**--*' * 5, '*--**' * 5))
             else:
